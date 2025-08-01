4707d79b6b406b5de3b78d6ddc801e86f6377734
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-01 15:38:28 +0300

    update: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./frontend/index.html",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./frontend/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {

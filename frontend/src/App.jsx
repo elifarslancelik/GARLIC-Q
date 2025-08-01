f70ea28dbd7aa510f813725f77de2163d8e8b500
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-01 15:36:37 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import LandingPage from '../components/LandingPage.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App 

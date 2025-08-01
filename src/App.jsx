7ba200c7d01897f2aab117c5c252a19be477b4a7
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-01 04:26:15 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import LandingPage from '../components/LandingPage.jsx'
+
+function App() {
+  return (
+    <div className="relative">
+      <Header />
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App

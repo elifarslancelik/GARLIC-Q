c4daa7095ae41ae7fc59ec38d8479c3990c8d8f8
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-05 02:12:21 +0300

    create codeService.js: code generation service

---
+++ frontend/src/services/codeService.js
@@ -0,0 +1,39 @@
+import { api } from '../utils/api';
+
+export const codeService = {
+  generateCode: async (prompt, language, maxTokens = 512, temperature = 0.3) => {
+    try {
+      const response = await api.generateCode(prompt, language, maxTokens, temperature);
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  translateCode: async (sourceCode, sourceLanguage, targetLanguage) => {
+    try {
+      const response = await api.translateCode(sourceCode, sourceLanguage, targetLanguage);
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  generateChat: async (message) => {
+    try {
+      const response = await api.generateChat(message);
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+}; 

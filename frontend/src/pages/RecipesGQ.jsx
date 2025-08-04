caeee374b5aa553a9a8a39c227d5b7806769e6af
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-05 01:26:40 +0300

    create RecipesGQ.jsx: recipes component

---
+++ frontend/src/pages/RecipesGQ.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RecipesGQ from '../components/features/RecipesGQ/index';
+
+const RecipesGQPage = () => {
+  return <RecipesGQ />;
+};
+
+export default RecipesGQPage; 

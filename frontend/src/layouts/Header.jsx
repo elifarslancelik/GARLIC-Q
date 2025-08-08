610e3243362f4b9816bdc8b96f150b2aeb0a76d9
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-08 20:38:44 +0300

    refactor: move logo to public, update Header.jsx

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../../assets/garliq.png';
 
 const Header = () => {
   return (
     <header className="absolute top-0 left-0 right-0 p-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <img src={logo} alt="GARLIC-Q Logo" className="w-8 h-8" />
+          <img src="/garliq.png" alt="GARLIC-Q Logo" className="w-8 h-8" />
           <span className="text-white text-apple font-light">GARLIC-Q</span>
         </div>
       </div>
